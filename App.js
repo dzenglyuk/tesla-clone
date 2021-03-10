816c631760a5fc811ad48d6593b51a0ffec73097
Oleh Dzenhliuk <dzenglyuk23@ukr.net>
2021-03-10 14:04:44 +0200

    Header created

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { StatusBar, StyleSheet, View } from "react-native";
 import CarsList from "./components/CarsList";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarsList />
       <StatusBar style="auto" />
     </View>
